6e17414074ac955250e95707b40b87edc4d95c5d
Ogu1208 <kdasunb@naver.com>
2023-12-26 11:35:59 +0900

    [feat] Article, File, Thesis 엔티티 구현

---
+++ src/main/java/com/umust/umustbe/File.java
@@ -0,0 +1,35 @@
+package com.umust.umustbe;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String origFilename;
+
+    @Column(nullable = false)
+    private String filename;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    @Builder
+    public File(Long id, String origFilename, String filename, String filePath) {
+        this.id = id;
+        this.origFilename = origFilename;
+        this.filename = filename;
+        this.filePath = filePath;
+    }
+}
