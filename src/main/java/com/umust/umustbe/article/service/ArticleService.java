51072e7644c13f355c03c54ec69ed862f97883ce
Ogu1208 <kdasunb@naver.com>
2024-01-07 22:43:28 +0900

    [refactor] Service method에 @Transactional 애너테이션 추가

--- src/main/java/com/umust/umustbe/article/service/ArticleService.java
+++ src/main/java/com/umust/umustbe/article/service/ArticleService.java
@@ -16,19 +16,25 @@ public class ArticleService {
 
     private final ArticleRepository articleRepository;
 
+    @Transactional
     public Article save(AddArticleRequest request) {
         return articleRepository.save(request.toEntity());
     }
 
+    /* READ 게시글 리스트 조회 readOnly 속성으로 조회속도 개선 */
+    @Transactional(readOnly = true)
     public List<Article> findAll() {
         return articleRepository.findAll();
     }
 
+    /* READ 게시글 리스트 조회 readOnly 속성으로 조회속도 개선 */
+    @Transactional(readOnly = true)
     public Article findById(long id) {
         return articleRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
     }
 
+    @Transactional
     public void delete(long id) {
         articleRepository.deleteById(id);
     }
