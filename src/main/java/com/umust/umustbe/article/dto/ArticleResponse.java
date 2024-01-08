db71b63bd214a43dbc6294e15950782f5cbcb821
Ogu1208 <kdasunb@naver.com>
2024-01-08 15:28:54 +0900

    [feat] Article API Swagger 문서화 테스트

--- src/main/java/com/umust/umustbe/article/dto/ArticleResponse.java
+++ src/main/java/com/umust/umustbe/article/dto/ArticleResponse.java
@@ -1,9 +1,11 @@
 package com.umust.umustbe.article.dto;
 
 import com.umust.umustbe.article.domain.Article;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "게시물 리스트 응답DTO")
 public record ArticleResponse (Long id,
                                String title,
                                String content,
