ff41e1d4d2fd3f1ef4cde9bbc983595141f4a6a1
Ogu1208 <kdasunb@naver.com>
2024-01-10 13:06:45 +0900

    [feat] 게시글 단건 조회시 조회수 1 증가

--- src/main/java/com/umust/umustbe/article/service/ArticleService.java
+++ src/main/java/com/umust/umustbe/article/service/ArticleService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -28,10 +29,17 @@ public class ArticleService {
     }
 
     /* READ 게시글 리스트 조회 readOnly 속성으로 조회속도 개선 */
-    @Transactional(readOnly = true)
+    @Transactional
     public Article findById(long id) {
-        return articleRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+        Optional<Article> article = articleRepository.findById(id);
+        if (article.isPresent()) {
+            Article viewedArticle = article.get();
+            viewedArticle.setView(viewedArticle.getView() + 1);
+            articleRepository.save(viewedArticle);
+            return viewedArticle;
+        } else {
+            throw new IllegalArgumentException("not found: " + id);
+        }
     }
 
     @Transactional
