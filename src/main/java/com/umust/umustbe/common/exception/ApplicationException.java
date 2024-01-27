5b39bd593d000628cbb6f60013a016a2c83cd022
Ogu1208 <kdasunb@naver.com>
2024-01-27 15:45:49 +0900

    [feat] RuntimeException 상속받는 ApplicationException 구현
    3개의 필드
    
    - `private final HttpStatus httpStatus;`
    - ` private final String errorCode;`
    - ` private final String message;`

---
+++ src/main/java/com/umust/umustbe/common/exception/ApplicationException.java
@@ -0,0 +1,25 @@
+package com.umust.umustbe.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApplicationException extends RuntimeException{
+    private final HttpStatus httpStatus;
+    private final String errorCode;
+    private final String message;
+
+    public ApplicationException(HttpStatus httpStatus) {
+        ExceptionType exceptionType = ExceptionType.from(this.getClass());
+        this.httpStatus = httpStatus;
+        this.errorCode = exceptionType.getErrorCode();
+        this.message = exceptionType.getMessage();
+    }
+
+    public ApplicationException(HttpStatus httpStatus, String optionalMessage) {
+        ExceptionType exceptionType = ExceptionType.from(this.getClass());
+        this.httpStatus = httpStatus;
+        this.errorCode = exceptionType.getErrorCode();
+        this.message = exceptionType.getMessage() + optionalMessage;
+    }
+}
