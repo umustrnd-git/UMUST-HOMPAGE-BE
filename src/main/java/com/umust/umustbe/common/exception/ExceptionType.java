77821afdd08829b4049ac8fe26bd32887038dd0d
Ogu1208 <kdasunb@naver.com>
2024-01-27 15:44:02 +0900

    [feat] Global ExceptionHandler, ExceptionType, ErrorResponse DTO 구현

---
+++ src/main/java/com/umust/umustbe/common/exception/ExceptionType.java
@@ -0,0 +1,44 @@
+package com.umust.umustbe.common.exception;
+
+import com.umust.umustbe.article.exception.ArticleCategoryNotFoundException;
+import com.umust.umustbe.article.exception.ArticleContentNullException;
+import com.umust.umustbe.article.exception.ArticleNotFoundException;
+import com.umust.umustbe.article.exception.ArticleTitleNullOrEmptyException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@AllArgsConstructor
+@Getter
+public enum ExceptionType {
+
+    /* Article Exception */
+    ARTICLE_NOT_FOUND_EXCEPTION("3001", "게시글이 존재하지 않습니다.", ArticleNotFoundException.class),
+    ARTICLE_TITLE_NULL_OR_EMPTY_EXCEPTION("3003", "게시글 제목은 비어있을 수 없습니다.", ArticleTitleNullOrEmptyException.class),
+    ARTICLE_CONTENT_NULL_EXCEPTION("3004", "게시글 내용은 null일 수 없습니다.", ArticleContentNullException.class),
+    ARTICLE_CATEGORY_NOT_FOUND_EXCEPTION("3005", "존재하지 않는 카테고리입니다.", ArticleCategoryNotFoundException.class),
+
+    /* Etc Exception */
+    METHOD_ARGUMENT_NOT_VALID_EXCEPTION("0001", "요청 데이터가 잘못되었습니다."),
+    UNHANDLED_EXCEPTION("0000", "알 수 없는 서버 에러가 발생했습니다.");
+
+
+    private final String errorCode;
+    private final String message;
+
+    private Class<? extends ApplicationException> type;
+
+    ExceptionType(String errorCode, String message) {
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+    public static ExceptionType from(Class<?> classType) {
+        return Arrays.stream(values())
+                .filter(it -> Objects.nonNull(it.type) && it.type.equals(classType))
+                .findFirst()
+                .orElse(UNHANDLED_EXCEPTION);
+    }
+}
---
+++ src/main/java/com/umust/umustbe/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.umust.umustbe.common.exception;
+
+import com.umust.umustbe.common.exception.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<ErrorResponse> applicationException(ApplicationException e) {
+        log.info(String.format("Application Exception : %s", e));
+        return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getErrorCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.info(String.format("MethodArgumentNotValidException : %s", e));
+        String errorCode = ExceptionType.METHOD_ARGUMENT_NOT_VALID_EXCEPTION.getErrorCode();
+        String message = e.getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity.badRequest().body(new ErrorResponse(errorCode, message));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> runtimeException(Exception e) {
+        StackTraceElement[] stackTrace = e.getStackTrace();
+        log.error(String.format("UnHandled Exception : %s\n" + "%s:%s:%s", e, stackTrace[0].getClassName(),
+                stackTrace[0].getMethodName(), stackTrace[0].getLineNumber()), e);
+        String message = ExceptionType.UNHANDLED_EXCEPTION.getMessage();
+        String errorCode = ExceptionType.UNHANDLED_EXCEPTION.getErrorCode();
+        return ResponseEntity.internalServerError().body(new ErrorResponse(errorCode, message));
+    }
+}
