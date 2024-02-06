b530b4158ab79667095a3164511744acfb60a5aa
Ogu1208 <kdasunb@naver.com>
2024-02-06 14:28:53 +0900

    [feat] article category : album 추가

--- src/main/java/com/umust/umustbe/article/type/ArticleCategory.java
+++ src/main/java/com/umust/umustbe/article/type/ArticleCategory.java
@@ -3,9 +3,8 @@ package com.umust.umustbe.article.type;
 public enum ArticleCategory {
     NOTICE("공지사항"),
     NEWS("보도자료"),
-    EVENT("행사정보")
-
-    ;
+    EVENT("행사정보"),
+    ALBUM("앨범");
 
     private final String category;
 
