70439f02cb017b68ae5da1076a47c15b5bf24645
Ogu1208 <kdasunb@naver.com>
2024-02-13 18:02:57 +0900

    [chore] SpringBoot timezone 설정 - ` @PostConstruct`

--- src/main/java/com/umust/umustbe/UmustbeApplication.java
+++ src/main/java/com/umust/umustbe/UmustbeApplication.java
@@ -1,10 +1,18 @@
 package com.umust.umustbe;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class UmustbeApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UmustbeApplication.class, args);
